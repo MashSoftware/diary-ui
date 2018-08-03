226f1f15269171a3fee9db47a89dd7898e20cebb
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2018-08-04 00:41:47 +0100

    add validators and help text

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length
 
 
 class UserForm(FlaskForm):
-    email_address = StringField('Email address', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    email_address = StringField('Email address', validators=[DataRequired(), Email()], description="We'll never share your email with anyone else.")
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)], description="Must be at least 8 characters long.")
     first_name = StringField('First name', validators=[DataRequired()])
     last_name = StringField('Last name', validators=[DataRequired()])
     submit = SubmitField('Create')
