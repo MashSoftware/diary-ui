f4fc7ad4a25c722aca8171f5db53b66c3786b6b0
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2018-08-05 23:20:00 +0100

    maximum password length

--- app/forms.py
+++ app/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class UserForm(FlaskForm):
     email_address = StringField('Email address', validators=[DataRequired(), Email()], description="We'll never share your email with anyone else.")
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)], description="Must be at least 8 characters long.")
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=72)], description="Must be between 8 and 72 characters long.")
     confirm_password = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password', message="Passwords must match.")])
     first_name = StringField('First name', validators=[DataRequired()])
     last_name = StringField('Last name', validators=[DataRequired()])
