4c8bc73bd98ed28c1a86a77df00e24b6c663dca8
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2018-08-06 23:26:46 +0100

    add register child form and route

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField
+from wtforms import (DateField, PasswordField, RadioField, StringField,
+                     SubmitField)
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 
@@ -9,4 +10,12 @@ class UserForm(FlaskForm):
     confirm_password = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password', message="Passwords must match.")])
     first_name = StringField('First name', validators=[DataRequired()])
     last_name = StringField('Last name', validators=[DataRequired()])
+    submit = SubmitField('Create')
+
+
+class ChildForm(FlaskForm):
+    first_name = StringField('First name', validators=[DataRequired()])
+    last_name = StringField('Last name', validators=[DataRequired()])
+    date_of_birth = DateField('Date of birth', validators=[DataRequired()])
+    gender = RadioField('Gender', validators=[DataRequired()], choices=[('male', 'Male'), ('female', 'Female')])
     submit = SubmitField('Create')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import flash, redirect, render_template, url_for
 
 from app import app
-from app.forms import UserForm
+from app.forms import ChildForm, UserForm
 
 
 @app.route('/')
@@ -18,3 +18,12 @@ def create_user():
         flash('Success!')
         return redirect(url_for('index'))
     return render_template('user.html', title='Create user', form=form)
+
+
+@app.route('/children', methods=['GET', 'POST'])
+def create_child():
+    form = ChildForm()
+    if form.validate_on_submit():
+        flash('Success!')
+        return redirect(url_for('index'))
+    return render_template('child.html', title='Register child', form=form)
