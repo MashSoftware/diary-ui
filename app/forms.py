e4b559a9f566e739f2aeb080fc6b8f141b68438c
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2018-08-07 00:04:08 +0100

    log in form and consistent language

--- app/forms.py
+++ app/forms.py
@@ -4,18 +4,24 @@ from wtforms import (DateField, PasswordField, RadioField, StringField,
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 
-class UserForm(FlaskForm):
+class SignUpForm(FlaskForm):
     email_address = StringField('Email address', validators=[DataRequired(), Email()], description="We'll never share your email with anyone else.")
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=72)], description="Must be between 8 and 72 characters long.")
     confirm_password = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password', message="Passwords must match.")])
     first_name = StringField('First name', validators=[DataRequired()])
     last_name = StringField('Last name', validators=[DataRequired()])
-    submit = SubmitField('Create')
+    submit = SubmitField('Sign up')
 
 
-class ChildForm(FlaskForm):
+class LogInForm(FlaskForm):
+    email_address = StringField('Email address', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log in')
+
+
+class RegisterChildForm(FlaskForm):
     first_name = StringField('First name', validators=[DataRequired()])
     last_name = StringField('Last name', validators=[DataRequired()])
     date_of_birth = DateField('Date of birth', validators=[DataRequired()])
     gender = RadioField('Gender', validators=[DataRequired()], choices=[('male', 'Male'), ('female', 'Female')])
-    submit = SubmitField('Create')
+    submit = SubmitField('Register')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import flash, redirect, render_template, url_for
 
 from app import app
-from app.forms import ChildForm, UserForm
+from app.forms import RegisterChildForm, SignUpForm, LogInForm
 
 
 @app.route('/')
@@ -11,19 +11,28 @@ def index():
     return render_template('index.html', title='Home', user=user)
 
 
-@app.route('/users', methods=['GET', 'POST'])
+@app.route('/signup', methods=['GET', 'POST'])
 def create_user():
-    form = UserForm()
+    form = SignUpForm()
     if form.validate_on_submit():
-        flash('Success!')
+        flash('{} {} signed up!'.format(form.first_name.data, form.last_name.data))
         return redirect(url_for('index'))
-    return render_template('user.html', title='Sign up', form=form)
+    return render_template('sign_up.html', title='Sign up', form=form)
 
 
-@app.route('/children', methods=['GET', 'POST'])
-def create_child():
-    form = ChildForm()
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LogInForm()
     if form.validate_on_submit():
-        flash('Success!')
+        flash('{} logged in'.format(form.email_address.data))
+        return redirect(url_for('index'))
+    return render_template('log_in.html', title='Sign in', form=form)
+
+
+@app.route('/register-child', methods=['GET', 'POST'])
+def create_child():
+    form = RegisterChildForm()
+    if form.validate_on_submit():
+        flash('Registered child!')
         return redirect(url_for('index'))
     return render_template('child.html', title='Register child', form=form)
