98bdeb72dc96154b0fd4be8216d706c1cc98e86e
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2018-08-07 00:39:36 +0100

    add remember me checkbox

--- app/forms.py
+++ app/forms.py
@@ -1,21 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import (DateField, PasswordField, RadioField, StringField,
-                     SubmitField)
+from wtforms import (BooleanField, DateField, PasswordField, RadioField,
+                     StringField, SubmitField)
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 
 class SignUpForm(FlaskForm):
+    first_name = StringField('First name', validators=[DataRequired()])
+    last_name = StringField('Last name', validators=[DataRequired()])
     email_address = StringField('Email address', validators=[DataRequired(), Email()], description="We'll never share your email with anyone else.")
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=72)], description="Must be between 8 and 72 characters long.")
     confirm_password = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password', message="Passwords must match.")])
-    first_name = StringField('First name', validators=[DataRequired()])
-    last_name = StringField('Last name', validators=[DataRequired()])
     submit = SubmitField('Sign up')
 
 
 class LogInForm(FlaskForm):
     email_address = StringField('Email address', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember me')
     submit = SubmitField('Log in')
 
 
