55c5a719909280e1b9d1b9cc31760e12f2b4a57b
Matt Shaw <matthew-shaw@users.noreply.github.com>
2018-08-08 00:03:57 +0100

    create user api call

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 import json
+
 import requests
-from flask import current_app
+from flask import Response, current_app
 
 base_url = current_app.config.get('DIARY_API_URL')
 version = current_app.config.get('DIARY_API_VERSION')
@@ -9,8 +10,35 @@ timeout = current_app.config.get('DIARY_API_TIMEOUT')
 
 class User(object):
 
-    def create(self, user):
-        pass
+    def create(self, password, first_name, last_name, email_address):
+        url = '{0}/{1}/users'.format(base_url, version)
+
+        new_user = {
+            "password": password,
+            "first_name": first_name,
+            "last_name": last_name,
+            "email_address": email_address
+        }
+
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+
+        try:
+            response = requests.post(url, data=json.dumps(new_user), headers=headers, timeout=timeout)
+        except requests.exceptions.Timeout:
+            return Response(response=json.dumps({"message": "Request Timeout"}, separators=(',', ':')),
+                            mimetype='application/json',
+                            status=408)
+        else:
+            if response.status_code != 201:
+                return Response(response=json.dumps({"message": "Failed to create user"}, separators=(',', ':')),
+                                mimetype='application/json',
+                                status=response.status_code)
+            else:
+                user = json.loads(response.text)
+                return user
 
     def search(self, query):
         pass
