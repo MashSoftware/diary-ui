2d0f9df2301421b621bc6fec719753ccb9e8e4ac
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2018-08-08 09:47:51 +0100

    flake8

--- app/forms.py
+++ app/forms.py
@@ -7,8 +7,10 @@ from wtforms.validators import DataRequired, Email, EqualTo, Length
 class SignUpForm(FlaskForm):
     first_name = StringField('First name', validators=[DataRequired()])
     last_name = StringField('Last name', validators=[DataRequired()])
-    email_address = StringField('Email address', validators=[DataRequired(), Email()], description="We'll never share your email with anyone else.")
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=72)], description="Must be between 8 and 72 characters long.")
+    email_address = StringField('Email address', validators=[DataRequired(), Email()],
+                                description="We'll never share your email with anyone else.")
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=72)],
+                             description="Must be between 8 and 72 characters long.")
     confirm_password = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password', message="Passwords must match.")])
     submit = SubmitField('Sign up')
 
