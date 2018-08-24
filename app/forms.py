84080df32289f62ca4bcf4f005ada326a9f3da81
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2018-08-24 14:07:58 +0100

    form validation messages

--- app/forms.py
+++ app/forms.py
@@ -5,23 +5,23 @@ from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 
 class SignUpForm(FlaskForm):
-    first_name = StringField('First name', validators=[DataRequired()])
-    last_name = StringField('Last name', validators=[DataRequired()])
-    email_address = StringField('Email address', validators=[DataRequired(), Email()],
+    first_name = StringField('First name', validators=[DataRequired(message="First name is required")])
+    last_name = StringField('Last name', validators=[DataRequired(message="Last name is required")])
+    email_address = StringField('Email address', validators=[DataRequired(message="Email address is required"), Email()],
                                 description="We'll never share your email with anyone else.")
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=72)],
+    password = PasswordField('Password', validators=[DataRequired(message="Password is required"), Length(min=8, max=72)],
                              description="Must be between 8 and 72 characters long.")
-    confirm_password = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password', message="Passwords must match.")])
+    confirm_password = PasswordField('Confirm password', validators=[DataRequired(message="Confirm password is required"), EqualTo('password', message="Passwords must match.")])
 
 
 class LogInForm(FlaskForm):
-    email_address = StringField('Email address', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    email_address = StringField('Email address', validators=[DataRequired(message="Email address is required"), Email()])
+    password = PasswordField('Password', validators=[DataRequired(message="Password is required")])
     remember_me = BooleanField('Remember me')
 
 
 class RegisterChildForm(FlaskForm):
-    first_name = StringField('First name', validators=[DataRequired()])
-    last_name = StringField('Last name', validators=[DataRequired()])
-    date_of_birth = DateField('Date of birth', validators=[DataRequired()])
-    gender = RadioField('Gender', validators=[DataRequired()], choices=[('male', 'Male'), ('female', 'Female')])
+    first_name = StringField('First name', validators=[DataRequired(message="First name is required")])
+    last_name = StringField('Last name', validators=[DataRequired(message="Last name is required")])
+    date_of_birth = DateField('Date of birth', validators=[DataRequired(message="Date of birth is required")])
+    gender = RadioField('Gender', validators=[DataRequired(message="Gender is required")], choices=[('male', 'Male'), ('female', 'Female')])
