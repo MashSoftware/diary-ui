69f91465381d92849fd99c07ffb935c05bdcba4f
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2018-09-03 13:21:31 +0100

    remove child gender

--- app/forms.py
+++ app/forms.py
@@ -24,4 +24,3 @@ class RegisterChildForm(FlaskForm):
     first_name = StringField('First name', validators=[InputRequired(message="First name is required")])
     last_name = StringField('Last name', validators=[InputRequired(message="Last name is required")])
     date_of_birth = DateField('Date of birth', validators=[InputRequired(message="Date of birth is required")])
-    gender = RadioField('Gender', validators=[InputRequired(message="Gender is required")], choices=[('male', 'Male'), ('female', 'Female')])
