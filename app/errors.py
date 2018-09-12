0a0aa044fc9c6cac0bd382e90620b4a121e9bdfa
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2018-09-12 16:30:00 +0100

    handle http 403

--- app/errors.py
+++ app/errors.py
@@ -3,11 +3,16 @@ from flask import render_template
 from app import app
 
 
+@app.errorhandler(403)
+def forbidden(error):
+    return render_template('error.html', title='Forbidden'), 403
+
+
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('error.html', title='Not found'), 404
+    return render_template('error.html', title='Page not found'), 404
 
 
 @app.errorhandler(500)
 def internal_server(error):
-    return render_template('error.html', title='Internal Server Error'), 500
+    return render_template('error.html', title='Internal server error'), 500
