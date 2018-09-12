c165c89d83a35c43a7d1a496832f61e7511a1c57
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2018-09-12 18:54:46 +0100

    handle all HTTPExceptions

--- app/errors.py
+++ app/errors.py
@@ -1,18 +1,9 @@
 from flask import render_template
+from werkzeug.exceptions import HTTPException
 
 from app import app
 
 
-@app.errorhandler(403)
+@app.errorhandler(HTTPException)
 def forbidden(error):
-    return render_template('error.html', title='Forbidden'), 403
-
-
-@app.errorhandler(404)
-def not_found(error):
-    return render_template('error.html', title='Page not found'), 404
-
-
-@app.errorhandler(500)
-def internal_server(error):
-    return render_template('error.html', title='Internal server error'), 500
+    return render_template('error.html', title='Error', error=error), error.code
