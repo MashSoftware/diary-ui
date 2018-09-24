96e7a76f9809224cb897dbb726a56ef6cc89bc92
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2018-09-24 23:18:33 +0100

    update profile and change password forms

--- app/forms.py
+++ app/forms.py
@@ -19,6 +19,20 @@ class LogInForm(FlaskForm):
     remember_me = BooleanField('Remember me')
 
 
+class ProfileForm(FlaskForm):
+    first_name = StringField('First name', validators=[InputRequired(message="First name is required")])
+    last_name = StringField('Last name', validators=[InputRequired(message="Last name is required")])
+    email_address = StringField('Email address', validators=[InputRequired(message="Email address is required"), Email()],
+                                description="We'll never share your email with anyone else.")
+
+
+class PasswordForm(FlaskForm):
+    current_password = PasswordField('Current password', validators=[InputRequired(message="Current password is required")])
+    new_password = PasswordField('New password', validators=[InputRequired(message="New password is required"), Length(min=8, max=72)],
+                             description="Must be between 8 and 72 characters long.")
+    confirm_password = PasswordField('Confirm password', validators=[InputRequired(message="Please confirm your password"), EqualTo('password', message="Passwords must match.")])
+
+
 class RegisterChildForm(FlaskForm):
     first_name = StringField('First name', validators=[InputRequired(message="First name is required")])
     last_name = StringField('Last name', validators=[InputRequired(message="Last name is required")])
