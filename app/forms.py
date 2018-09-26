1c4eed4b331a83ee82902b2fc29f93af35745cfa
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2018-09-26 23:40:57 +0100

    rename and reuse child form, format code and imports

--- app/forms.py
+++ app/forms.py
@@ -10,11 +10,13 @@ class UserForm(FlaskForm):
                                 description="We'll never share your email with anyone else.")
     password = PasswordField('Password', validators=[InputRequired(message="Password is required"), Length(min=8, max=72)],
                              description="Must be between 8 and 72 characters long.")
-    confirm_password = PasswordField('Confirm password', validators=[InputRequired(message="Please confirm your password"), EqualTo('password', message="Passwords must match.")])
+    confirm_password = PasswordField('Confirm password', validators=[InputRequired(
+        message="Please confirm your password"), EqualTo('password', message="Passwords must match.")])
 
 
 class LogInForm(FlaskForm):
-    email_address = StringField('Email address', validators=[InputRequired(message="Email address is required"), Email()])
+    email_address = StringField('Email address', validators=[
+                                InputRequired(message="Email address is required"), Email()])
     password = PasswordField('Password', validators=[InputRequired(message="Password is required")])
     remember_me = BooleanField('Remember me')
 
@@ -27,13 +29,15 @@ class ProfileForm(FlaskForm):
 
 
 class PasswordForm(FlaskForm):
-    current_password = PasswordField('Current password', validators=[InputRequired(message="Current password is required")])
+    current_password = PasswordField('Current password', validators=[
+                                     InputRequired(message="Current password is required")])
     new_password = PasswordField('New password', validators=[InputRequired(message="New password is required"), Length(min=8, max=72)],
                                  description="Must be between 8 and 72 characters long.")
-    confirm_password = PasswordField('Confirm password', validators=[InputRequired(message="Please confirm your password"), EqualTo('password', message="Passwords must match.")])
+    confirm_password = PasswordField('Confirm password', validators=[InputRequired(
+        message="Please confirm your password"), EqualTo('password', message="Passwords must match.")])
 
 
-class RegisterChildForm(FlaskForm):
+class ChildForm(FlaskForm):
     first_name = StringField('First name', validators=[InputRequired(message="First name is required")])
     last_name = StringField('Last name', validators=[InputRequired(message="Last name is required")])
     date_of_birth = DateField('Date of birth', validators=[InputRequired(message="Date of birth is required")])
