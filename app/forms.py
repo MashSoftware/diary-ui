ca29f3cd283fedd36b8f98d96d9f4ea5975c7825
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2018-10-04 23:52:00 +0100

    middle name and validate date of birth

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,11 @@
-from datetime import datetime
+from datetime import date, datetime
 
 from flask_wtf import FlaskForm
 from wtforms import (BooleanField, DateField, DateTimeField, DecimalField,
                      PasswordField, RadioField, SelectField, StringField,
                      TextAreaField)
-from wtforms.validators import Email, EqualTo, InputRequired, Length, Optional
+from wtforms.validators import (Email, EqualTo, InputRequired, Length,
+                                Optional, ValidationError)
 
 
 class UserForm(FlaskForm):
@@ -43,19 +44,27 @@ class PasswordForm(FlaskForm):
 
 class ChildForm(FlaskForm):
     first_name = StringField('First name', validators=[InputRequired(message="First name is required")])
+    middle_name = StringField('Middle name', validators=[Optional()])
     last_name = StringField('Last name', validators=[InputRequired(message="Last name is required")])
     date_of_birth = DateField('Date of birth', validators=[InputRequired(message="Date of birth is required")])
 
+    def validate_date_of_birth(self, date_of_birth):
+        if date_of_birth.data > date.today():
+            raise ValidationError('Date of birth must be in the past')
+
 
 class UserSearchForm(FlaskForm):
-    email_address = StringField('Email address', validators=[InputRequired(message="Email address is required"), Email()])
+    email_address = StringField('Email address', validators=[
+                                InputRequired(message="Email address is required"), Email()])
 
 
 class EventForm(FlaskForm):
-    type = SelectField('Type', choices=[('sleep', 'Sleep'), ('feed', 'Feed'), ('change', 'Change')])
-    started_at = DateTimeField('Started at', format='%d/%m/%Y %H:%M:%S', default=datetime.utcnow)
-    ended_at = DateTimeField('Ended at', format='%d/%m/%Y %H:%M:%S')
+    type = SelectField('Type', choices=[('sleep', 'Sleep'), ('feed', 'Feed'),
+                                        ('change', 'Change')], validators=[InputRequired(message="Type is required")])
+    started_at = DateTimeField('Started at', format='%d/%m/%Y %H:%M:%S',
+                               validators=[InputRequired(message="Started at is required")], default=datetime.utcnow)
+    ended_at = DateTimeField('Ended at', format='%d/%m/%Y %H:%M:%S', validators=[Optional()])
     amount = DecimalField('Amount', validators=[Optional()])
     unit = StringField('Unit', validators=[Optional()])
-    side = RadioField('Side', choices=[('left', 'Left'), ('right', 'Right')])
-    description = TextAreaField('Description', validators=[Optional()])
+    side = RadioField('Side', choices=[('left', 'Left'), ('right', 'Right')], validators=[Optional()])
+    notes = TextAreaField('Notes', validators=[Optional()])
